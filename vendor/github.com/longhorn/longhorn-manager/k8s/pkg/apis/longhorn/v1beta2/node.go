b57bf2c42b192c93f74c35710e8b76bfa3dfd82d
Vicente Cheng <vicente.cheng@suse.com>
2026-03-04 11:13:30 +0800

    common: bump golang/k8s/bci image
        - bump golang 1.25.7
        - k8s 1.34.5
        - bci image 16.0
    
    Signed-off-by: Vicente Cheng <vicente.cheng@suse.com>

--- vendor/github.com/longhorn/longhorn-manager/k8s/pkg/apis/longhorn/v1beta2/node.go
+++ vendor/github.com/longhorn/longhorn-manager/k8s/pkg/apis/longhorn/v1beta2/node.go
@@ -3,12 +3,14 @@ package v1beta2
 import metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 
 const (
-	NodeConditionTypeReady              = "Ready"
-	NodeConditionTypeMountPropagation   = "MountPropagation"
-	NodeConditionTypeMultipathd         = "Multipathd"
-	NodeConditionTypeRequiredPackages   = "RequiredPackages"
-	NodeConditionTypeNFSClientInstalled = "NFSClientInstalled"
-	NodeConditionTypeSchedulable        = "Schedulable"
+	NodeConditionTypeReady               = "Ready"
+	NodeConditionTypeMountPropagation    = "MountPropagation"
+	NodeConditionTypeMultipathd          = "Multipathd"
+	NodeConditionTypeKernelModulesLoaded = "KernelModulesLoaded"
+	NodeConditionTypeRequiredPackages    = "RequiredPackages"
+	NodeConditionTypeNFSClientInstalled  = "NFSClientInstalled"
+	NodeConditionTypeSchedulable         = "Schedulable"
+	NodeConditionTypeHugePagesAvailable  = "HugePagesAvailable"
 )
 
 const (
@@ -22,10 +24,14 @@ const (
 	NodeConditionReasonMultipathdIsRunning       = "MultipathdIsRunning"
 	NodeConditionReasonUnknownOS                 = "UnknownOS"
 	NodeConditionReasonNamespaceExecutorErr      = "NamespaceExecutorErr"
+	NodeConditionReasonKernelModulesNotLoaded    = "KernelModulesNotLoaded"
 	NodeConditionReasonPackagesNotInstalled      = "PackagesNotInstalled"
-	NodeConditionReasonKernelConfigIsNotFound    = "KernelConfigIsNotFound"
+	NodeConditionReasonCheckKernelConfigFailed   = "CheckKernelConfigFailed"
 	NodeConditionReasonNFSClientIsNotFound       = "NFSClientIsNotFound"
+	NodeConditionReasonNFSClientIsMisconfigured  = "NFSClientIsMisconfigured"
 	NodeConditionReasonKubernetesNodeCordoned    = "KubernetesNodeCordoned"
+	NodeConditionReasonHugePagesNotConfigured    = "HugePagesNotConfigured"
+	NodeConditionReasonInsufficientHugePages     = "InsufficientHugePages"
 )
 
 const (
@@ -43,17 +49,20 @@ const (
 )
 
 const (
-	ErrorReplicaScheduleInsufficientStorage              = "insufficient storage"
-	ErrorReplicaScheduleDiskNotFound                     = "disk not found"
-	ErrorReplicaScheduleDiskUnavailable                  = "disks are unavailable"
-	ErrorReplicaScheduleSchedulingSettingsRetrieveFailed = "failed to retrieve scheduling settings failed to retrieve"
-	ErrorReplicaScheduleTagsNotFulfilled                 = "tags not fulfilled"
-	ErrorReplicaScheduleNodeNotFound                     = "node not found"
-	ErrorReplicaScheduleNodeUnavailable                  = "nodes are unavailable"
-	ErrorReplicaScheduleEngineImageNotReady              = "none of the node candidates contains a ready engine image"
-	ErrorReplicaScheduleHardNodeAffinityNotSatisfied     = "hard affinity cannot be satisfied"
-	ErrorReplicaScheduleSchedulingFailed                 = "replica scheduling failed"
-	ErrorReplicaSchedulePrecheckNewReplicaFailed         = "precheck new replica failed"
+	ErrorReplicaScheduleInsufficientStorage               = "insufficient storage"
+	ErrorReplicaScheduleDiskNotFound                      = "disk not found"
+	ErrorReplicaScheduleDiskUnavailable                   = "disks are unavailable"
+	ErrorReplicaScheduleTagsNotFulfilled                  = "tags not fulfilled"
+	ErrorReplicaScheduleNodeNotFound                      = "node not found"
+	ErrorReplicaScheduleNodeUnavailable                   = "nodes are unavailable"
+	ErrorReplicaScheduleEngineImageNotReady               = "none of the node candidates contains a ready engine image"
+	ErrorReplicaScheduleHardNodeAffinityNotSatisfied      = "hard affinity cannot be satisfied"
+	ErrorReplicaScheduleLinkedCloneNotSatisfied           = "linked clone replica cannot be satisfied"
+	ErrorReplicaScheduleSchedulingFailed                  = "replica scheduling failed"
+	ErrorReplicaScheduleUnusedFailedReplicaIsNotSupported = "unused failed replica is not supported"
+	ErrorReplicaScheduleReplicaAlreadyScheduled           = "replica already scheduled"
+	ErrorReplicaScheduleLonghornClientOperationFailed     = "longhorn client operation failed"
+	ErrorReplicaScheduleIncompatibleVolumeSize            = "incompatible volume size"
 )
 
 type DiskType string
@@ -71,6 +80,7 @@ const (
 	DiskDriverNone = DiskDriver("")
 	DiskDriverAuto = DiskDriver("auto")
 	DiskDriverAio  = DiskDriver("aio")
+	DiskDriverNvme = DiskDriver("nvme")
 )
 
 type SnapshotCheckStatus struct {
@@ -84,7 +94,7 @@ type DiskSpec struct {
 	Type DiskType `json:"diskType"`
 	// +optional
 	Path string `json:"path"`
-	// +kubebuilder:validation:Enum="";auto;aio
+	// +kubebuilder:validation:Enum="";auto;aio;nvme
 	// +optional
 	DiskDriver DiskDriver `json:"diskDriver"`
 	// +optional
@@ -110,6 +120,9 @@ type DiskStatus struct {
 	// +optional
 	// +nullable
 	ScheduledReplica map[string]int64 `json:"scheduledReplica"`
+	// +optional
+	// +nullable
+	ScheduledBackingImage map[string]int64 `json:"scheduledBackingImage"`
 	// +optional
 	DiskUUID string `json:"diskUUID"`
 	// +optional
